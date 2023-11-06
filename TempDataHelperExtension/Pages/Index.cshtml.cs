49a1e08842d1d09c073765a6b130733fe9e81f7e
Tomáš Kazda <tomas.kazda@pslib.cz>
2023-11-06 22:09:18 +0100

    add cookie

--- TempDataHelperExtension/Pages/Index.cshtml.cs
+++ TempDataHelperExtension/Pages/Index.cshtml.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace TempDataHelper.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        public string LastAccess { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -14,7 +17,14 @@ namespace TempDataHelper.Pages
 
         public void OnGet()
         {
-
+            LastAccess = Request.Cookies["last-access"] ?? DateTime.Now.ToString();
+            var options = new CookieOptions {
+                Expires = DateTime.Now.AddDays(365),
+                HttpOnly = false,
+                Secure = false,
+            };
+            Response.Cookies.Append("last-access", DateTime.Now.ToString(), options);
         }
     }
+
 }
